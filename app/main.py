6c48eeb8084c7fa495af015fd7c7a858f8bdd521
Jdiegomaya <juandiegomayamorales@gmail.com>
2024-10-24 15:22:57 -0500

    actualizamos

--- app/main.py
+++ app/main.py
@@ -1,42 +1,6 @@
-from fastapi import FastAPI, HTTPException 
-from app.models.models import Animal, Mamifero, Ave
-from app.services.services import AnimalService
+from fastapi import FastAPI
+from .controllers import animal_controller
 
 app = FastAPI()
 
-animal_service = AnimalService()
-
-
-@app.post("/animales/")
-def agregar_animal(animal: Animal):
-    animal_service.agregar_animal(animal)
-    return {"message": "Animal agregado correctamente."}
-
-
-@app.get("/animales/")
-def obtener_animales():
-    return animal_service.obtener_animales()
-
-
-@app.get("/animales/{nombre}")
-def obtener_animal(nombre: str):
-    animal = animal_service.obtener_animal_por_nombre(nombre)
-    if animal:
-        return animal
-    raise HTTPException(status_code=404, detail="Animal no encontrado")
-
-
-@app.put("/animales/{nombre}")
-def actualizar_animal(nombre: str, animal_actualizado: Animal):
-    actualizado = animal_service.actualizar_animal(nombre, animal_actualizado)
-    if actualizado:
-        return {"message": "Animal actualizado correctamente."}
-    raise HTTPException(status_code=404, detail="Animal no encontrado")
-
-
-@app.delete("/animales/{nombre}")
-def eliminar_animal(nombre: str):
-    eliminado = animal_service.eliminar_animal(nombre)
-    if eliminado:
-        return {"message": "Animal eliminado correctamente."}
-    raise HTTPException(status_code=404, detail="Animal no encontrado")
+app.include_router(animal_controller.router)
